dbab4e029413bd0dce82f83baef978bc0f3ce91e
gleb_from_home <home@mail.com>
2022-01-02 00:02:16 +0300

    Text of program only

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import sys
 import random
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QPainter, QColor
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("ylw.ui", self)
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Git и случайные окружности')
+        self.btn = QPushButton('Сделай круг', self)
+        self.btn.resize(120, 40)
+        self.btn.move(180, 40)
         self.painting = False
         self.btn.clicked.connect(self.do)
 
@@ -26,7 +29,7 @@ class Window(QWidget):
     def drawing(selfself, qp):
         size = random.randint(10, 50)
         x, y = random.randint(10, 450), random.randint(65, 450)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         qp.drawEllipse(x, y, size, size)
 
 if __name__ == '__main__':
